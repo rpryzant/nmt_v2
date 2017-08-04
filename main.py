5bc7d95bfc709d2e7ed9b41916fa57aa9b3be466
Reid Pryzant <reidpryzant@Reids-MacBook-Pro.local>
2017-08-04 15:48:41 -0700

    share vocab bug fix

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ import train
 
 
 def process_command_line():
-  """
-  Return a 1-tuple: (args list).
-  `argv` is a list of arguments, or `None` for ``sys.argv[1:]``.
-  """
+    """
+    Return a 1-tuple: (args list).
+    `argv` is a list of arguments, or `None` for ``sys.argv[1:]``.
+    """
 
-  parser = argparse.ArgumentParser(description='usage') # add description
-  parser.add_argument('--gpu', dest='gpu', type=str, default='0', help='gpu')
-  args = parser.parse_args()
-  return args
+    parser = argparse.ArgumentParser(description='usage') # add description
+    parser.add_argument('--gpu', dest='gpu', type=str, default='0', help='gpu')
+    args = parser.parse_args()
+    return args
 
 
 
@@ -35,11 +35,14 @@ def load_config(filename):
         src_vocab_file, c.data_dir, c.sos, c.eos, c.unk)
     c = c._replace(src_vocab_size=src_vocab_size)
 
-    if not c.share_vocab:
+    if c.share_vocab:
+        c = c._replace(tgt_vocab_size=src_vocab_size)
+    else:
         tgt_vocab_file = os.path.join(c.data_dir, c.vocab_prefix + "." + c.tgt)
         tgt_vocab_file, tgt_vocab_size = utils.check_vocab(
             tgt_vocab_file, c.data_dir, c.sos, c.eos, c.unk)
         c = c._replace(tgt_vocab_size=tgt_vocab_size)
+
 
     if not os.path.exists(c.out_dir):
         os.makedirs(c.out_dir)
